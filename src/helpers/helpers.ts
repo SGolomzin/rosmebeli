653a2025ea13a9ae5509ef1eb8e54d0a2f5d357f
SGolomzin <s.golomzin@gmail.com>
2024-08-14 00:04:14 +0300

    Добавил функцию для создания базового URL для ссылок на сайте, теперь перед билдом необходимо делать меньше исправлений

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -4,3 +4,10 @@ export const declOfNum = (number: number, titles: [string, string, string]): str
 	const cases = [2, 0, 1, 1, 1, 2]
 	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]]
 };
+
+export const baseUrl = (url: string) => {
+	const site = import.meta.env.DEV ? 'http://localhost:4321' : import.meta.env.SITE;
+	const base = import.meta.env.BASE_URL;
+	const lastSlash = base === '/' ? '' : '/';
+	return new URL(url, `${site}${base}${lastSlash}`).href;
+}
